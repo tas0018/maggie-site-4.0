194eafbe1bfed0371c1c2a2b2092c41c7723312a
Taylor Smith <tas0018@auburn.edu>
2021-12-14 12:44:36 -0600

    Initialized the porject and added tailwind css

---
+++ maggie-site-v-4/src/pages/index.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <Seo title="Home" />
+    <h1 className="text-red-700">Hi people</h1>
+  </Layout>
+)
+
+export default IndexPage
