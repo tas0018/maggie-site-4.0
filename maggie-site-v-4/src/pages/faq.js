9b7185a836d2b253e511b2f1a15353fe381a79a2
Taylor Smith <tas0018@auburn.edu>
2022-03-13 13:04:49 -0500

    Changing the faq page questions to the figma design

--- maggie-site-v-4/src/pages/faq.js
+++ maggie-site-v-4/src/pages/faq.js
@@ -12,23 +12,34 @@ const FAQ = () => (
     <Seo title="FAQ" />
     <NavbarFAQ />
     <FaqHero />
+
     <div className="flex h-max w-screen py-20">
-      <div className="flex flex-col pl-24 pr-6 w-6/12">
-        <div>
-          <h2 className="text-cyan-700 text-xl font-medium py-8">
-            Do you accept insurance?
-          </h2>
-          <p className="text-lg text-slate-500 border-b pb-8  border-cyan-700  ">
+      <div className="flex flex-col px-4 w-full items-center">
+        <div className="w-4/5 bg-gray-100 rounded-md mb-8">
+          <div className="flex flex-row justify-between items-center">
+            <h2 className="p-8 text-cyan-900 text-xl font-medium">
+              Do you accept insurance?
+            </h2>
+            <div className="p-8">
+              <p className="text-2xl font-medium text-cyan-900">+</p>
+            </div>
+          </div>
+          <p className="px-8 pb-8 text-[#475569] font-medium">
             I do not given my current licensure, but if you have BlueCross
             BlueShield I accept what would be considered your copay of $30.
           </p>
         </div>
 
-        <div>
-          <h2 className="text-cyan-700 text-xl font-medium py-8">
-            Is therapy right for me?
-          </h2>
-          <p className="text-lg text-slate-500 border-b pb-8  border-cyan-700  ">
+        <div className="w-4/5 bg-gray-100 rounded-md mb-8">
+          <div className="flex flex-row justify-between items-center">
+            <h2 className="p-8 text-cyan-900 text-xl font-medium">
+              Is therapy right for me?
+            </h2>
+            <div className="p-8">
+              <p className="text-2xl font-medium">+</p>
+            </div>
+          </div>
+          <p className="px-8 pb-8 text-[#475569] font-medium">
             Yes, therapy is for everyone. All of us could be better in the way
             we love ourselves and the world around us. We learn how to better
             love ourself and others by unlearning thoughts and behaviors that we
@@ -36,24 +47,32 @@ const FAQ = () => (
           </p>
         </div>
 
-        <div>
-          <h2 className="text-cyan-700 text-xl font-medium py-8">
-            What is therapy like?
-          </h2>
-          <p className="text-lg text-slate-500 border-b pb-8  border-cyan-700  ">
+        <div className="w-4/5 bg-gray-100 rounded-md mb-8">
+          <div className="flex flex-row justify-between items-center">
+            <h2 className="p-8 text-cyan-900 text-xl font-medium">
+              What is therapy like?
+            </h2>
+            <div className="p-8">
+              <p className="text-2xl font-medium">+</p>
+            </div>
+          </div>
+          <p className="px-8 pb-8 text-[#475569] font-medium">
             Therapy is a unique experience for everyone because it is
             relational. Therapy is a place where you can come as you are and be
             seen and heard.
           </p>
         </div>
-      </div>
 
-      <div className="flex flex-col pr-24 pl-6 w-6/12">
-        <div>
-          <h2 className="text-cyan-700 text-xl font-medium py-8">
-            What techniques or styles is your practice?
-          </h2>
-          <p className="text-lg text-slate-500 border-b pb-8  border-cyan-700  ">
+        <div className="w-4/5 bg-gray-100 rounded-md mb-8">
+          <div className="flex flex-row justify-between items-center">
+            <h2 className="p-8 text-cyan-900 text-xl font-medium">
+              What techniques or styles is your practice?
+            </h2>
+            <div className="p-8">
+              <p className="text-2xl font-medium">+</p>
+            </div>
+          </div>
+          <p className="px-8 pb-8 text-[#475569] font-medium">
             I have a very empathetic style. I borrow techniques and language for
             Dialectical Behavior Therapy, Cognitive Behavior Therapy, and Person
             Centered Therapy. In my practice I help build awareness and
@@ -62,11 +81,16 @@ const FAQ = () => (
           </p>
         </div>
 
-        <div>
-          <h2 className="text-cyan-700 text-xl font-medium py-8">
-            How will therapy help you?
-          </h2>
-          <p className="text-lg text-slate-500 border-b pb-8  border-cyan-700  ">
+        <div className="w-4/5 bg-gray-100 rounded-md mb-8">
+          <div className="flex flex-row justify-between items-center">
+            <h2 className="p-8 text-cyan-900 text-xl font-medium">
+              How will therapy help you?
+            </h2>
+            <div className="p-8">
+              <p className="text-2xl font-medium">+</p>
+            </div>
+          </div>
+          <p className="px-8 pb-8 text-[#475569] font-medium">
             Therapy will help you have more awareness of self and help you
             navigate life's triggers. Therapy teaches us to be mindful and to be
             present to our lives so we become more responsive rather than
