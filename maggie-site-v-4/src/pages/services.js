eb96c9175fc62444c051be869f30c60dcb31e011
Taylor Smith <tas0018@auburn.edu>
2022-04-10 17:43:15 -0500

    started first services tab

--- maggie-site-v-4/src/pages/services.js
+++ maggie-site-v-4/src/pages/services.js
@@ -2,8 +2,10 @@ import React from "react"
 import Footer from "../components/Footer"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import { Link } from "gatsby"
 import NavbarServices from "../components/NavbarServices"
 import ServicesHero from "../components/ServicesHero"
+import ServicesImgHeader from "../images/ServicesImgHeader.png"
 
 function services() {
   return (
@@ -13,7 +15,7 @@ function services() {
       <Seo title="Waters Therapy Services" />
       <ServicesHero />
       <div className="flex flex-row w-full h-max py-8 px-12">
-        <div className="flex flex-col w-4/12 pl-24">
+        <div className="flex flex-col w-4/12 pl-28 pr-4">
           <div className=" flex flex-row items-center bg-[#214B59] text-[1.8rem] rounded-md border border-[#E7E7E7] h-12 mb-2">
             <h2 className="text-white ml-8">Services</h2>
           </div>
@@ -66,7 +68,29 @@ function services() {
             <h3 className="text-[#556575] ml-8">Grief</h3>
           </div>
         </div>
-        <div className="flex flex-col w-8/12"></div>
+        <div className="flex flex-col w-8/12 pr-28 pl-8">
+          <div>
+            <div
+              className="w-full h-64 rounded-md bg-cover"
+              style={{ backgroundImage: `url(${ServicesImgHeader})` }}
+            >
+              <div className="h-full w-3/5 flex flex-col p-8 justify-around">
+                <h2 className="text-white text-[1.8rem]">Services</h2>
+                <p className="text-white text-sm">
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi
+                  quis illo et quo! Vero eligendi enim nesciunt Lorem ipsum
+                  dolor sit amet consectetur adipisicing elit.?
+                </p>
+                <Link
+                  className="text-white py-2 px-8 bg-[#214B59] rounded-sm w-fit font-medium text-sm hover:bg-[#F2F2F2] "
+                  to="/"
+                >
+                  Book Now
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <Footer></Footer>
     </div>
